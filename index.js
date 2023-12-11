e3858878f0d218cbbf18a1e78200403e1509bb4c
Brian Babu <briansmkenya@gmail.com>
2023-12-11 16:09:41 +0300

    added completed task and remove task functionalities

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ function addTask() {
     if (input.value.trim() === ``) {
         alert(`input value is empty: please write something`)
     } else {
-       const taskContent = input.value.charAt(0).toUpperCase()
-       const concatTaskContent = taskContent + input.value.slice(1)
-       const newTask = document.createElement('li')
-       newTask.innerText = concatTaskContent
-       taskContainer.appendChild(newTask)
+        const taskContent = input.value.charAt(0).toUpperCase()
+        const concatTaskContent = taskContent + input.value.slice(1)
+        const newTask = document.createElement('li')
+        const span = document.createElement('span')
+        span.innerHTML = "&times;"
+        newTask.innerText = concatTaskContent
+        taskContainer.appendChild(newTask)
+        newTask.appendChild(span)
     }
     input.value = ''
 }
+
+taskContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked')
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove()
+    }
+}, false)
