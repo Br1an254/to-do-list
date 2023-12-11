083d081bc0273b26c31d47b8d2a856417598c988
Brian Babu <briansmkenya@gmail.com>
2023-12-11 14:55:39 +0300

    new add functionalities

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const input = document.getElementById('input')
 const taskContainer = document.querySelector('.task-container')
 
 function addTask() {
-    const newTask = document.createElement('li')
-    newTask.innerText = input.value
-    taskContainer.appendChild(newTask)
+
+    if (input.value.trim() === ``) {
+        alert(`input value is empty: please write something`)
+    } else {
+       const taskContent = input.value.charAt(0).toUpperCase()
+       const concatTaskContent = taskContent + input.value.slice(1)
+       const newTask = document.createElement('li')
+       newTask.innerText = concatTaskContent
+       taskContainer.appendChild(newTask)
+    }
     input.value = ''
 }
